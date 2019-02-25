019162e6ecddc17281cf6432da3afdaa49b282cd
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-02-25 12:00:43 -0500

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello , Dee!" );
+
+        int myFirstNumber = 10 + 5;
+
+        int mySecondNumber = 12;
+
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+
+        System.out.println(1000 - myTotal);
+
+    }
+}
